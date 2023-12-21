19e5e5d5d46ce4663fe7444000e340a5fc9b689e
jt-hunter <108434400+jt-hunter@users.noreply.github.com>
2023-12-21 17:56:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 from predictions import get_predictions
 
 app = Flask(__name__)
@@ -7,7 +8,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     predict_17hr, predict_15hr, predict_8hr, predict_4hr = get_predictions()
-    return render_template('app.html', value1 = predict_17hr, value2 = predict_15hr, value3 = predict_8hr, \
+    time = str(datetime.datetime.now()
+    return render_template('app.html', time = time, value1 = predict_17hr, value2 = predict_15hr, value3 = predict_8hr, \
                            value4 = predict_4hr)
 
 
