01c6d215dd948a0c348d59bbea323a81e0a7f38f
jt-hunter <108434400+jt-hunter@users.noreply.github.com>
2023-12-21 17:52:28 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from predictions import get_predictions
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    predict_17hr, predict_15hr, predict_8hr, predict_4hr = get_predictions()
+    return render_template('app.html', value1 = predict_17hr, value2 = predict_15hr, value3 = predict_8hr, \
+                           value4 = predict_4hr)
+
+
+if __name__ == '__main__':
+    app.run()
